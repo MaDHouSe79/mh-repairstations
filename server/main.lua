a34959634f6376115357706858626666503db081
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-31 00:32:47 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,23 +4,23 @@ local function SocietyAccount(amount)
     end
 end
 
-QBCore.Functions.CreateCallback('qb-carfixstation:server:pay', function(source, cb, amount)
+QBCore.Functions.CreateCallback('qb-carfixstation:server:pay', function(source, cb, cost)
     if QBCore.Functions.GetPlayer(source).Functions.RemoveMoney("cash", amount, "repair-station") then
 	if state then
-	    SocietyAccount(amount)
+	    SocietyAccount(cost)
 	end
 	cb({
 	    state   = true,
-	    message = Lang:t('company.cost_repair', {amount})
+	    message = Lang:t('company.cost_repair', {amount = cost})
 	})
     else
 	if QBCore.Functions.GetPlayer(source).Functions.RemoveMoney("bank", amount, "repair-station") then
 	    if state then
-	    	SocietyAccount(amount)
+	    	SocietyAccount(cost)
 	    end
 	    cb({
 		state   = true,
-		message = Lang:t('company.cost_repair', {amount})
+		message = Lang:t('company.cost_repair', {amount = cost})
 	    })
 	else
 	    cb({
