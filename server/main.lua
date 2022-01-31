c4897e53feea48eb9512164d69c74abdc6076756
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-31 05:00:09 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -7,22 +7,22 @@ end
 QBCore.Functions.CreateCallback('qb-carfixstation:server:pay', function(source, cb, cost)
     if QBCore.Functions.GetPlayer(source).Functions.RemoveMoney("cash", cost, "repair-station") then
 	SocietyAccount(cost)
+	cb({
+	    state   = true,
+	    message = Lang:t('company.cost_repair', {amount = cost})
+	})
+    else
+	if QBCore.Functions.GetPlayer(source).Functions.RemoveMoney("bank", cost, "repair-station") then
+	    SocietyAccount(cost)
 	    cb({
 		state   = true,
 		message = Lang:t('company.cost_repair', {amount = cost})
 	    })
 	else
-	    if QBCore.Functions.GetPlayer(source).Functions.RemoveMoney("bank", cost, "repair-station") then
-		SocietyAccount(cost)
-		cb({
-		    state   = true,
-		    message = Lang:t('company.cost_repair', {amount = cost})
-		})
-	    else
-		cb({
-		    state   = false,
-		    message = Lang:t('company.not_enough_money')
-		})
-	    end
+	    cb({
+		state   = false,
+		message = Lang:t('company.not_enough_money')
+	    })
 	end
+    end
 end)
