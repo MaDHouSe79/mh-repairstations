662dedd418afaed8b90e60d562b6de46db887332
MaDHouSe79 <madhouse1979@gmail.com>
2022-03-28 09:46:18 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -2,20 +2,21 @@ local Translations = {
     system = {
         ['repaired']            = "Voertuig is gerepareerd",
         ['press_repair_free']   = 'Druk op ~INPUT_PICKUP~ om het voertuig gratis repareren',
-	    ['repair_finish']       = 'Voertuig is gerepareerd!',
-	    ['repair_processing']   = 'Reparatie bezig, even geduld a.u.b...',
+	['repair_finish']       = 'Voertuig is gerepareerd!',
+	['repair_processing']   = 'Reparatie bezig, even geduld a.u.b...',
     },
 
     company = {
 	    ['name']                = "Autoreparatiestation",
 	    ['not_enough_money']    = 'Je hebt niet genoeg geld',
 	    ['cost_repair']         = 'Je hebt €%{amount} uitgegeven om het voertuig te repareren',
-	},
+	    ['done_repair']         = 'Je voertuig is gerepareerd',
+    },
 
-	info = {
-	    ['press_repair_free']   = 'Druk op ~INPUT_PICKUP~ to repair vehicle for free',
-	    ['press_repair_cost']   = 'Druk op ~INPUT_PICKUP~ kosten €%{amount} om het voertuig repareren',
-	},
+    info = {
+	['press_repair_free']   = 'Druk op ~INPUT_PICKUP~ to repair vehicle for free',
+	['press_repair_cost']   = 'Druk op ~INPUT_PICKUP~ kosten €%{amount} om het voertuig repareren',
+    },
 }
 
 Lang = Locale:new({
