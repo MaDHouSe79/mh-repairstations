910d68cced226fc467c9bc1b72bbe5059a856ffb
MaDHouSe79 <madhouse1979@gmail.com>
2022-03-28 09:46:44 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -2,21 +2,21 @@ local Translations = {
     system = {
        	['repaired']            = "Vehicle has been repaired",
         ['press_repair_free']   = 'Press ~INPUT_PICKUP~ to repair vehicle for free',
-		['repair_finish']       = 'Vehicle has been repaired!',
-		['repair_processing']   = 'Repair in progress, please wait...',
+	['repair_finish']       = 'Vehicle has been repaired!',
+	['repair_processing']   = 'Repair in progress, please wait...',
     },
 
     company = {
-		['name']                = "Car Repair Station",
-		['not_enough_money']    = 'You don\'t have enough cash',
-		['cost_repair']         = 'You spent €%{amount} to repair vehicle',
-		['done_repair']         = 'Your vehicle has been repaired',
-	},
+	['name']                = "Car Repair Station",
+	['not_enough_money']    = 'You don\'t have enough cash',
+	['cost_repair']         = 'You spent €%{amount} to repair vehicle',
+	['done_repair']         = 'Your vehicle has been repaired',
+    },
 
-	info = {
-		['press_repair_free']   = 'Press ~INPUT_PICKUP~ to repair vehicle for free',
-		['press_repair_cost']   = 'Press ~INPUT_PICKUP~ cost €%{amount} to repair vehicle',
-	},
+    info = {
+	['press_repair_free']   = 'Press ~INPUT_PICKUP~ to repair vehicle for free',
+	['press_repair_cost']   = 'Press ~INPUT_PICKUP~ cost €%{amount} to repair vehicle',
+    },
 }
 
 Lang = Locale:new({
